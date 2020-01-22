02a44d48cea08645e9691eaa1dbbd7e9010aab19
huynonstop <tthuykh99@gmail.com>
2020-01-22 18:16:37 +0700

    Updates

---
+++ precache-manifest.ab9be6d5b385c064d9bb94aaf5326734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f7e6edd03e90de59fef0927f2d422a",
+    "url": "/scroll-image/index.html"
+  },
+  {
+    "revision": "6d2826fb6388f13d9378",
+    "url": "/scroll-image/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "356677ddc3bf4d6f26c6",
+    "url": "/scroll-image/static/js/2.101e58be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scroll-image/static/js/2.101e58be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d2826fb6388f13d9378",
+    "url": "/scroll-image/static/js/main.33417806.chunk.js"
+  },
+  {
+    "revision": "3fc89f139532adaaa414",
+    "url": "/scroll-image/static/js/runtime-main.1fd12c6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scroll-image/static/media/logo.5d5d9eef.svg"
+  }
+]);
